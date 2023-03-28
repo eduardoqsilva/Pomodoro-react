99041e910e23b1ba59aef2b5180e370015b1bf5b
eduardoqsilva <eduadsilva@outlook.com>
2023-03-28 16:06:15 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Buttons } from "./components/Buttons"
 import { Header } from "./components/Header"
 import { Pomodoro } from "./components/Pomodoro"
+import { useTimer } from "./hooks/useTimer"
 
 function App() {
 
   const [active, setActive] = useState(0)
+  const {time, func} = useTimer([1,6,15] ,active)
 
   return (
     <Header>
       <h1>pomodoro</h1>
       <Buttons active={active} setActive={setActive}/>
       <Pomodoro 
-        porcent={90}
-        min={10}
-        sec={0}
-        status={'start'}
+        porcent={time.porcent}
+        min={time.minutes}
+        sec={time.seconds}
+        status={time.status}
       />
+      <button onClick={() => func.pause()}>pause</button>
+      <button onClick={() => func.play()}>play</button>
     </Header>
   )
 }
