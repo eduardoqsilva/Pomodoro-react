5ef55f50c28a064e48b41974b5bbc6bee0f7e977
eduardoqsilva <eduadsilva@outlook.com>
2023-03-29 18:32:05 -0300

    fix errors

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,32 +1,38 @@
 import { useEffect, useState } from "react";
+import { CreateNotification } from "../utils/notification";
+
 import bell from "../assets/sound/bell.aac"
+import icon from "/icon.png"
 
 interface useTimeType {
   time: {
     minutes: number
     seconds: number
     porcent: number
+    completed: number
     status: string
   }
   func: {
-    play: () => void
-    pause: () => void
-    start: () => void
+    playAndPause: () => void
   }
 }
 
 const sound = new Audio(bell)
+let timeOutId:number
 
 export function useTimer(p:Array<number>, active:number):useTimeType {
 
   const min = p[active]
+  const secondsTotal = min * 60
     
   const [timer, setTimer] = useState({
     minutes: 0,
     seconds: 0,
     porcent: 100.0,
+    completed: 6,
     status: 'start'
   })
+  const [isPause, setIsPause] = useState(false)
 
   useEffect(() => {
     setTimer(prev => ({
@@ -35,52 +41,84 @@ export function useTimer(p:Array<number>, active:number):useTimeType {
     }))
   }, [active])
 
+  const reset = () => {
+    if(timer.completed === 7) {
+      setTimer({
+        ...timer,
+        completed: 0
+      })
+    }
+    setTimer(prev => ({
+      ...prev,
+      minutes: min,
+      porcent: 100.0,
+      completed: prev.completed + 1,
+      status: 'start'
+    }))
+  }
+
   let pomodoro = {
-    isPause: false,
-    intervalId: 0,
     seconds: timer.seconds,
     start: function() {
-      this.intervalId = setInterval(() => {
-        if(this.isPause !== true) {
-          if(this.seconds === 0) {
-            if(timer.minutes === 0) {
-              clearInterval(this.intervalId)
-              console.log('acabou!')
-              sound.play()
-            }else {
-              this.seconds = 10
-              setTimer(prev => (
-                {
-                  ...prev, 
-                  minutes: timer.minutes -= 1,
-                  seconds: this.seconds,
-                }
-              ))
-            }
-          } else {
-            this.seconds -= 1
+
+      timeOutId = setInterval(() => {
+        if(this.seconds === 0) {
+          if(timer.minutes === 0) {
+            clearInterval(timeOutId)
+            reset()
+            setIsPause(false)
+            sound.play()
+            CreateNotification('Tempo esgotado!', {
+              icon: icon
+            })
+          }else {
+            this.seconds = 1
             setTimer(prev => (
               {
                 ...prev, 
+                minutes: timer.minutes -= 1,
                 seconds: this.seconds,
+                porcent: (((timer.minutes * 60) + this.seconds) / secondsTotal) * 100
               }
             ))
           }
+        } else {
+          this.seconds -= 1 
+          setTimer(prev => (
+            {
+              ...prev, 
+              seconds: this.seconds,
+              porcent: (((timer.minutes * 60) + this.seconds) / secondsTotal) * 100
+            }
+          ))
         }
-      }, 1000)
+      },1000)
+    },
+    stop: () => {
+      clearInterval(timeOutId)
+      console.log('id ' + timeOutId)
     }
   }
+  
 
 
-  const pause = () => {
-    pomodoro.isPause = true
-  }
-  const play = () => {
-    pomodoro.isPause = false
-    pomodoro.start()
-  }
-  const start = () => {
-    pomodoro.start()
+  function playAndPause() {
+    setIsPause(prev => (!prev))
+    if(isPause === true) {
+      pomodoro.stop()
+      console.log('pause ' + isPause)
+      setTimer({
+        ...timer,
+        status: 'continue'
+      })
+    }else {
+      pomodoro.start()
+      console.log('play')
+      setTimer({
+        ...timer,
+        status: 'pause'
+      })
+    }
   }
 
   return {
@@ -88,12 +126,11 @@ export function useTimer(p:Array<number>, active:number):useTimeType {
       minutes: timer.minutes,
       seconds: timer.seconds,
       porcent: timer.porcent,
+      completed: timer.completed,
       status: timer.status,
     },
     func: {
-      play,
-      pause,
-      start
+      playAndPause,
     }
   }
 }
